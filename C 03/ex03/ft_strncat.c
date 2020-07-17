5ec8f97f33a19c1c7ae0a8c530eed931a06ed747
Kyeongnam Kim <kyekim@c3r7s7.42seoul.kr>
2020-07-18 07:44:57 +0900

    Add C 03

---
+++ C 03/ex03/ft_strncat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyekim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 13:32:14 by kyekim            #+#    #+#             */
+/*   Updated: 2020/07/14 01:03:31 by kyekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int				digit;
+	unsigned int	i;
+	unsigned int	j;
+
+	digit = 0;
+	while (dest[digit] != '\0')
+	{
+		digit++;
+	}
+	i = digit;
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
