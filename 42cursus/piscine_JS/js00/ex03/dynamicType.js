8e3930f8a400b679d7e8a8aaa9ba5fbb0a4b1335
heeheeeee <dreaming_life23@naver.com>
2021-08-16 23:28:21 +0900

    JS00

--- 42cursus/piscine_JS/js00/ex03/dynamicType.js
+++ 42cursus/piscine_JS/js00/ex03/dynamicType.js
@@ -1,19 +1,31 @@
 const dynamicType = {
-	data = undefined,
-	put: function(num) {
-	    if (this.data == undefined)
-   	    this.num = input;
-	}
-	change: function(type) {
-	    if (this.data != undefined) {
-	        it (typeof type == 'string') {
-		    if (type == 'String') {
-		        if (Array.isArray(this.data) == true)
-			    this.data = String(this.data[0]);
-		    }
-		}
-	},
-	printType: function() {
-		console.log(type of 
-	}
-};
+  input: ``,
+  output: ``,
+  put: function (param) {
+    if (typeof param !== 'number')
+      throw `Not a number!`;
+    this.input = param;
+  },
+  change: function (type) {
+    if (this.input === '')
+      throw `\'put\' function is not called yet!`
+    switch (type) {
+      case 'String':
+        this.output = `` + this.input;
+        break;
+      case 'Number':
+        this.output = this.input;
+        break;
+      case 'Object':
+        this.output = { Number: this.input };
+        break;
+      case 'Array':
+        this.output = [this.input];
+    }
+  },
+  printType: function () {
+    if (this.input === '')
+      throw `\'put\' function is not called yet!`
+    console.log(this.output);
+  }
+}
